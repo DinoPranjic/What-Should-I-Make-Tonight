621f353f8e67ea2bd2106ad565e910c7f95563a7
Dino <dinopranjic92@gmail.com>
2023-02-04 19:51:09 -0500

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export type Recipe = {
+  name: string;
+  image: string;
+  origin: string;
+  category: string;
+  source: string;
+  youtube: string;
+  instructions: string;
+  ingredients: string[];
+  measurements: string[];
+}
+
